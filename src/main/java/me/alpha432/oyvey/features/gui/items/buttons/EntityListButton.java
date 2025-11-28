5019c12d61d41563234d22aff51a7441501c9b2c
analtforgames69-hue <analtforgames69@gmail.com>
2025-11-28 23:43:41 +0100

    Update EntityListButton.java

--- src/main/java/me/alpha432/oyvey/features/gui/items/buttons/EntityListButton.java
+++ src/main/java/me/alpha432/oyvey/features/gui/items/buttons/EntityListButton.java
@@ -1,55 +1,42 @@
 package me.alpha432.oyvey.features.gui.items.buttons;
 
-import me.alpha432.oyvey.OyVey;
-import me.alpha432.oyvey.features.gui.items.Item;
-import me.alpha432.oyvey.features.modules.Module;
 import me.alpha432.oyvey.features.modules.combat.Killaura;
-import me.alpha432.oyvey.features.modules.client.ClickGui;
+import me.alpha432.oyvey.features.setting.Setting;
 import me.alpha432.oyvey.util.render.RenderUtil;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.sound.PositionedSoundInstance;
 import net.minecraft.sound.SoundEvents;
 
-import java.util.ArrayList;
 import java.util.List;
 
-public class EntityListButton extends Item {
-
+public class EntityListButton extends Button {
     private final Killaura module;
-    private final List<String> entities = new ArrayList<>();
-    private boolean extended;
+    private boolean extended = false;
 
-    public EntityListButton(Module module) {
-        super("Entities");
-        if (!(module instanceof Killaura)) {
-            throw new IllegalArgumentException("EntityListButton can only be used with Killaura!");
-        }
-        this.module = (Killaura) module;
+    public EntityListButton(String name, Killaura module) {
+        super(name);
+        this.module = module;
         this.height = 15;
-
-        // default selection
-        if (this.module.targetPlayer.getValue()) entities.add("Player");
-        if (this.module.targetPhantom.getValue()) entities.add("Phantom");
     }
 
     @Override
     public void drawScreen(DrawContext context, int mouseX, int mouseY, float partialTicks) {
-        int color = !this.isHovering(mouseX, mouseY) ? OyVey.colorManager.getColorWithAlpha(OyVey.moduleManager.getModuleByClass(ClickGui.class).hoverAlpha.getValue())
-                : OyVey.colorManager.getColorWithAlpha(OyVey.moduleManager.getModuleByClass(ClickGui.class).alpha.getValue());
+        // Draw the main button
+        RenderUtil.rect(context.getMatrices(), this.x, this.y, this.x + (float) this.width,
+                this.y + (float) this.height - 0.5f,
+                this.isHovering(mouseX, mouseY) ? 0x77111111 : 0x55111111);
+        drawString(this.getName(), this.x + 2.3f, this.y - 2.0f, -1);
 
-        RenderUtil.rect(context.getMatrices(), this.x, this.y, this.x + this.width, this.y + this.height - 0.5f, color);
-
-        // Main label
-        drawString(context, "Entities Selected: " + entities.size() + " +", this.x + 2.3f, this.y - 2.0f - OyVey.getTextManager().getFontHeight(), -1);
-
-        // Draw dropdown if extended
+        // Draw extended entity toggles
         if (extended) {
-            int yOffset = 12;
-            for (String entity : List.of("Player", "Phantom")) {
-                boolean selected = entities.contains(entity);
-                int optionColor = selected ? 0xFF00FF00 : 0xFFAAAAAA;
-                drawString(context, entity, this.x + 5f, this.y + yOffset, optionColor);
-                yOffset += 12;
+            List<BooleanButton> entityButtons = module.getEntityButtons();
+            float offsetY = this.y + this.height;
+            for (BooleanButton btn : entityButtons) {
+                btn.x = this.x;
+                btn.y = offsetY;
+                btn.width = this.width;
+                btn.drawScreen(context, mouseX, mouseY, partialTicks);
+                offsetY += btn.getHeight();
             }
         }
     }
@@ -57,44 +44,25 @@ public class EntityListButton extends Item {
     @Override
     public void mouseClicked(int mouseX, int mouseY, int mouseButton) {
         if (mouseButton == 0 && this.isHovering(mouseX, mouseY)) {
+            this.extended = !this.extended;
             mc.getSoundManager().play(PositionedSoundInstance.master(SoundEvents.UI_BUTTON_CLICK, 1f));
-            extended = !extended;
         }
 
-        if (extended && mouseButton == 0) {
-            int yOffset = 12;
-            for (String entity : List.of("Player", "Phantom")) {
-                if (isHoveringOption(mouseX, mouseY, yOffset)) {
-                    toggleEntity(entity);
-                }
-                yOffset += 12;
+        if (extended) {
+            for (BooleanButton btn : module.getEntityButtons()) {
+                btn.mouseClicked(mouseX, mouseY, mouseButton);
             }
         }
     }
 
-    private void toggleEntity(String entity) {
-        if (entities.contains(entity)) {
-            entities.remove(entity);
-            setModuleValue(entity, false);
-        } else {
-            entities.add(entity);
-            setModuleValue(entity, true);
-        }
-    }
-
-    private void setModuleValue(String entity, boolean value) {
-        switch (entity) {
-            case "Player" -> module.targetPlayer.setValue(value);
-            case "Phantom" -> module.targetPhantom.setValue(value);
-        }
-    }
-
-    private boolean isHoveringOption(int mouseX, int mouseY, int yOffset) {
-        return mouseX >= this.x && mouseX <= this.x + this.width && mouseY >= this.y + yOffset && mouseY <= this.y + yOffset + 12;
-    }
-
     @Override
     public int getHeight() {
-        return extended ? 15 + 24 : 15; // 15 for main button + 12px per entity option
+        int baseHeight = 14;
+        if (extended) {
+            for (BooleanButton btn : module.getEntityButtons()) {
+                baseHeight += btn.getHeight();
+            }
+        }
+        return baseHeight;
     }
 }
