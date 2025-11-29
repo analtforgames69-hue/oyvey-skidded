7461b46209c473fac513d6888523c6dabeeeeabd
analtforgames69-hue <analtforgames69@gmail.com>
2025-11-29 13:12:21 +0100

    Update EntityListButton.java

--- src/main/java/me/alpha432/oyvey/features/gui/items/buttons/EntityListButton.java
+++ src/main/java/me/alpha432/oyvey/features/gui/items/buttons/EntityListButton.java
@@ -1,42 +1,56 @@
 package me.alpha432.oyvey.features.gui.items.buttons;
 
-import me.alpha432.oyvey.features.gui.items.buttons.Button;
-import me.alpha432.oyvey.features.modules.combat.Killaura;
+import me.alpha432.oyvey.features.settings.Setting;
+import me.alpha432.oyvey.features.gui.items.Item;
+import me.alpha432.oyvey.features.modules.Module;
+
 import net.minecraft.client.gui.DrawContext;
-import me.alpha432.oyvey.util.ColorUtil;
-import me.alpha432.oyvey.OyVey;
 
-public class EntityListButton extends Button {
+public class EntityListButton extends Item {
+    private final Setting<Target> setting;
+    private boolean extended;
 
-    private final Killaura killaura;
+    public enum Target {
+        PLAYER,
+        PHANTOM
+    }
 
-    public EntityListButton(Killaura ka) {
-        super("Target");
-        this.killaura = ka;
-        this.height = 14; // match your other buttons
-        this.width = 50;  // reasonable default width
+    public EntityListButton(Setting<Target> setting) {
+        super(setting.getName());
+        this.setting = setting;
+        this.height = 14;
     }
 
     @Override
     public void drawScreen(DrawContext context, int mouseX, int mouseY, float partialTicks) {
-        int color = ColorUtil.toARGB(255, 255, 255, 150); // simple hover effect
-        if (this.isHovering(mouseX, mouseY)) {
-            color = ColorUtil.toARGB(255, 255, 255, 255);
-        }
-
-        // Draw button background
-        RenderUtil.rect(context.getMatrices(), this.x, this.y, this.x + this.width, this.y + this.height, color);
-
-        // Draw the text: current target
-        drawString("Target: " + killaura.targetEntity, this.x + 2.3f, this.y + 2.0f, -1);
+        String display = this.setting.getName() + ": " + this.setting.getValue().name();
+        drawString(display, this.x + 2.0f, this.y + 1.0f, -1);
     }
 
     @Override
-    public void onMouseClick() {
-        if (killaura.targetEntity.equals("Player")) {
-            killaura.targetEntity = "Phantom";
-        } else {
-            killaura.targetEntity = "Player";
+    public void mouseClicked(int mouseX, int mouseY, int mouseButton) {
+        if (!isHovering(mouseX, mouseY)) return;
+        if (mouseButton == 0) {
+            // Left click cycles forward
+            Target[] values = Target.values();
+            int next = (this.setting.getValue().ordinal() + 1) % values.length;
+            this.setting.setValue(values[next]);
+        } else if (mouseButton == 1) {
+            // Right click cycles backward
+            Target[] values = Target.values();
+            int prev = (this.setting.getValue().ordinal() - 1 + values.length) % values.length;
+            this.setting.setValue(values[prev]);
         }
+    }
+
+    @Override
+    public int getHeight() {
+        return 14;
+    }
+
+    @Override
+    public boolean isHovering(int mouseX, int mouseY) {
+        return mouseX >= this.getX() && mouseX <= this.getX() + this.getWidth()
+                && mouseY >= this.getY() && mouseY <= this.getY() + this.height;
     }
 }
