7dcbd475de8fb189b0d3e9ff445ab9e2d9ede874
analtforgames69-hue <analtforgames69@gmail.com>
2025-11-29 13:27:40 +0100

    Update MathUtil.java

--- src/main/java/me/alpha432/oyvey/util/MathUtil.java
+++ src/main/java/me/alpha432/oyvey/util/MathUtil.java
@@ -51,6 +51,34 @@ public class MathUtil
         return MathHelper.wrapDegrees(value);
     }
 
+    public static float smoothRotation(float current, float target, float speed) {
+        float delta = wrapDegrees(target - current);
+        if (delta > speed) delta = speed;
+        if (delta < -speed) delta = -speed;
+        return current + delta;
+    }
+
+    // Already have wrapDegrees(float), but here is a safe version for smoothRotation
+    public static float wrapDegrees(float value) {
+        value = value % 360.0F;
+        if (value >= 180.0F) value -= 360.0F;
+        if (value < -180.0F) value += 360.0F;
+        return value;
+    }
+
+    // Calculate yaw/pitch from one entity to another (or any Vec3d)
+    public static float[] calculateLookAt(double x, double y, double z, Entity fromEntity) {
+        double diffX = x - fromEntity.getX();
+        double diffY = y - (fromEntity.getY() + fromEntity.getEyeHeight(fromEntity.getPose()));
+        double diffZ = z - fromEntity.getZ();
+        double dist = Math.sqrt(diffX * diffX + diffZ * diffZ);
+
+        float yaw = (float) Math.toDegrees(Math.atan2(diffZ, diffX)) - 90f;
+        float pitch = (float) -Math.toDegrees(Math.atan2(diffY, dist));
+        return new float[]{yaw, pitch};
+    }
+}
+
     public static double wrapDegrees(double value) {
         return MathHelper.wrapDegrees(value);
     }
