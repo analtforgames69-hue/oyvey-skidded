88a2e95e6d0a813682546c41f0819c4ab5c9dc67
analtforgames69-hue <analtforgames69@gmail.com>
2025-12-02 18:46:37 +0100

    Update PlayerInventoryAccessor.java

--- src/main/java/me/alpha432/oyvey/mixin/PlayerInventoryAccessor.java
+++ src/main/java/me/alpha432/oyvey/mixin/PlayerInventoryAccessor.java
@@ -6,7 +6,6 @@ import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(PlayerInventory.class)
 public interface PlayerInventoryAccessor {
-
     @Accessor("selectedSlot")
     int getSelectedSlot();
 
